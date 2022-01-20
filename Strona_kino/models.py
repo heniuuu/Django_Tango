f423659b8fb256cfd2e75e60fd71f50b2440cd85
heniuuu <98039815+heniuuu@users.noreply.github.com>
2022-01-21 00:45:41 +0100

    Filmy model chyba działa

--- Strona_kino/models.py
+++ Strona_kino/models.py
@@ -18,3 +18,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Film(models.Model):
+    tytul_filmu = models.CharField(max_length=200)
+    id_filmu = models.CharField(max_length=200)
+    cena_biletu = models.CharField(max_length=100)
+    ocena = models.CharField(max_length=100)
+    opis = models.TextField()
+
+    def __str__(self):
+        return self.tytul_filmu
+        # to robi że w panelu  zamiast obiekt film będzie nazwa filmu
