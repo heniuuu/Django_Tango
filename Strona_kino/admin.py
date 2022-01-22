2a4aaa052009f9694a9249e20322cd326bbb1d1c
heniuuu <98039815+heniuuu@users.noreply.github.com>
2022-01-22 17:55:43 +0100

    bilety
    w modelach jest zachaszowane co trzeba zrobić z biletem

--- Strona_kino/admin.py
+++ Strona_kino/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-
-from .models import Film
+from .models import Film , Bilet
 
 #admin.site.register(Film)
+
+
 @admin.register(Film)
 class FilmAdmin(admin.ModelAdmin):
     list_display = ['tytul','kategoria', 'jezyk', 'status', 'rok_produkcji','wyswietlenia' ]
+
+
+@admin.register(Bilet)
+class BiletAdmin(admin.ModelAdmin):
+    list_display = ['rodzaj_biletu','cena_biletu','ilosc_biletow']
--- Strona_kino/models.py
+++ Strona_kino/models.py
@@ -16,15 +16,29 @@ STATUS_CHOICES = (
     ('NO', 'NAJCZĘŚCIEJ OGLĄDANE'),
     ('TR', 'NAJLEPIEJ OCENIANE')
 )
+BILET_CHOICE = (
+    ('N','NORMALNY'),
+    ('U','ULGOWY'),
+    ('R','RODZINNY'),
+)
+
 class Film(models.Model):
     tytul = models.CharField(max_length=100)
     opis = models.TextField(max_length=1000)
     obraz = models.ImageField(upload_to='Strona_kino')
-    kategoria = models.CharField(choices=CATEGORY_CHOICE , max_length=1)
-    jezyk = models.CharField(choices=LANGUAGE_CHOICE , max_length=2)
-    status = models.CharField(choices=STATUS_CHOICES , max_length=2)
+    kategoria = models.CharField(choices=CATEGORY_CHOICE, max_length=1)
+    jezyk = models.CharField(choices=LANGUAGE_CHOICE, max_length=2)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=2)
     rok_produkcji = models.IntegerField(default=0)
     wyswietlenia = models.IntegerField(default=0)
 
     def __str__(self):
         return self.tytul
+
+class Bilet(models.Model):
+    rodzaj_biletu = models.CharField(choices=BILET_CHOICE, max_length=10)
+    cena_biletu = models.IntegerField(default=0)
+    ilosc_biletow = models.IntegerField(default=0)
+    #film = id(Film)
+    #to ma brać konkretny film ale nwm jak to zrobić pog
+
