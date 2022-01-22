b2dd3a9d76f00d83e9402c88b1bdb0eeb7c0d013
heniuuu <98039815+heniuuu@users.noreply.github.com>
2022-01-22 22:55:01 +0100

    urle

--- Strona_kino/urls.py
+++ Strona_kino/urls.py
@@ -1,14 +1,10 @@
 from . import views
-from django.contrib import admin
+
 from django.urls import path, include
-from .views import FilmsLista , FilmsDetale
+
 urlpatterns = [
     path('', views.main_site, name='main_site'),
     path('login', views.login_site, name='login_site')
 ]
 
 
-urlpatterns1 = [
-    path('', FilmsLista.as_view() , name='Films_Lista'),
-    path('<int:pk>', FilmsDetale.as_view() , name='Films_Detale'),
-]
