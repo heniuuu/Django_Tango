f595e1e1e9451925c9a552484c6bb8be81dc61ac
heniuuu <98039815+heniuuu@users.noreply.github.com>
2022-01-22 15:47:30 +0100

    admin.py wyświetlanie w panelu pełnych informacji

--- Strona_kino/admin.py
+++ Strona_kino/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Film
 
-admin.site.register(Film)
+#admin.site.register(Film)
+@admin.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+    list_display = ['tytul','kategoria', 'jezyk', 'status', 'rok_produkcji','wyswietlenia' ]
